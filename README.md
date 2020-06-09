8f7b579f95e91c6437b6c9c4462cb3b67069cd72
Kestutis Simaitis <kestutis.simaitis@geniussports.com>
2020-06-09 17:38:25 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node-Axios_API_test
---
+++ test/test.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const chai = require('chai');
+const expect = chai.expect;
+
+const API_URL = 'http://ptsv2.com/t/fu807-1554722621/post';
+
+async function makeGetRequest() {
+  try {
+    return await axios.get(API_URL);
+  } catch (error) {
+    throw new Error('Something went wrong, check if API is responding')
+  }
+}
+
+describe('A call to get user credentials', function () {
+
+  it('should return correct response code ', async function () {
+    let response = await makeGetRequest();
+    expect(response).to.have.property('status', 200);
+  });
+
+  it('should return correct Content type header', async function () {
+    let response = await makeGetRequest().then(function (response) {
+      return response.headers;
+    });
+    expect(response).to.have.property('content-type', 'text/html; charset=utf-8');
+  });
+
+  it('should return correct data in body', async function () {
+    let response = await makeGetRequest().then(function (response) {
+      return response.data;
+    });
+    expect(response).to.have.property('username', 'automate');
+    expect(response).to.have.property('password', 'everything');
+    expect(response).to.have.property('targetUrl', 'http://ptsv2.com/t/7ty82-1554722743/post');
+  });
+});
